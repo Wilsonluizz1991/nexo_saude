b1ead12035ac65b1a1d1d9f88ff7039d3a0969e3
wilsonluiz1991 <wilsonluiz@icloud.com>
2026-06-01 13:07:42 -0300

    fix: força atualização do favicon

--- resources/views/components/favicon.blade.php
+++ resources/views/components/favicon.blade.php
@@ -1,4 +1,5 @@
-<link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
-<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
-<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
-<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+<link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}?v=20260601">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}?v=20260601">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}?v=20260601">
+<link rel="shortcut icon" href="{{ asset('favicon.ico') }}?v=20260601">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}?v=20260601">
