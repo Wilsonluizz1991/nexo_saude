b869e5307ad66adff4f0b87b2e86e97048582b9a
wilsonluiz1991 <wilsonluiz@icloud.com>
2026-06-02 14:59:33 -0300

    fix: corrige logo de favicon e preview social

--- resources/views/components/favicon.blade.php
+++ resources/views/components/favicon.blade.php
@@ -1,5 +1,23 @@
-<link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}?v=20260601">
-<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}?v=20260601">
-<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}?v=20260601">
-<link rel="shortcut icon" href="{{ asset('favicon.ico') }}?v=20260601">
-<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}?v=20260601">
+@php
+    $faviconVersion = '20260602';
+    $socialImage = asset('assets/nexo-og-image.png') . '?v=' . $faviconVersion;
+@endphp
+
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}?v={{ $faviconVersion }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}?v={{ $faviconVersion }}">
+<link rel="shortcut icon" type="image/png" href="{{ asset('favicon-32x32.png') }}?v={{ $faviconVersion }}">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}?v={{ $faviconVersion }}">
+<meta property="og:type" content="website">
+<meta property="og:site_name" content="Nexo Saúde">
+<meta property="og:title" content="Nexo Saúde">
+<meta property="og:description" content="Plataforma Nexo Saúde para corretores de planos de saúde.">
+<meta property="og:url" content="{{ url()->current() }}">
+<meta property="og:image" content="{{ $socialImage }}">
+<meta property="og:image:secure_url" content="{{ $socialImage }}">
+<meta property="og:image:type" content="image/png">
+<meta property="og:image:width" content="1200">
+<meta property="og:image:height" content="630">
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:title" content="Nexo Saúde">
+<meta name="twitter:description" content="Plataforma Nexo Saúde para corretores de planos de saúde.">
+<meta name="twitter:image" content="{{ $socialImage }}">
